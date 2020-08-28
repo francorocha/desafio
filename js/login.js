9d8d5db9bf91954cc254ce42d3be7d3d3f268b56
Franco Rocha <68879664+francorocha@users.noreply.github.com>
2020-08-27 21:34:52 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,21 +3,18 @@ const chk_remember = document.getElementById("chk_remember")
 const email = document.getElementById("email");
 const pwd = document.getElementById("pwd");
 
-form.addEventListener("submit", function (event) {
-	event.preventDefault()
-	let usuario = Array({
-		correo: email.value,
-		pass: pwd.value
-    });
-    if(chk_remember.checked == true){
-        localStorage.setItem('usuario', JSON.stringify(usuario));
-        location.href='./index.html';
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+
+    if(chk_remember.cheched == true){
+        localStorage.setItem('usuario', email.value);
+        location.href="index.html";
     }
     else{
-        sessionStorage.setItem('usuario', JSON.stringify(usuario));
-        location.href='./index.html';
+        sessionStorage.setItem('usuario', email.value);
+        location.href="index.html";
     }
-});
+})
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
